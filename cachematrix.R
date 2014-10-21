661e1d5a7154b94a14c8d954f3ca014366e67480
Gary Malouf <gary@garymalouf.com>
2014-10-21 09:12:29 -0400

    Supporting comments for the cachematrix.R script.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are intended to be used to together to compute 
+## and cache the inverse of an invertible matrix.  
 
-## Write a short comment describing this function
+## This function handles the storing and retrieval of a matrix and it's inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
@@ -18,7 +18,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function works with the list object produced by 'makecachematrix'
+## to compute and cache the inverse of the matrix stored within it.
+## If the inverse of the matrix is already computed, it is retrieved from
+## the list object rather than re-computed.
 
 cacheSolve <- function(x, ...) {
     i <- x$getinverse()
